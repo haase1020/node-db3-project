e09089b7925801a9288acf3372aeeb0a39c5de38
Mandi Haase <haase1020@gmail.com>
2020-03-19 04:35:02 -0400

    MVP again

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,5 +1,54 @@
 const db = require('../data/db-config.js');
 
+
+
+const find = () => {
+    return db('schemes');
+};
+
+const findById = id => {
+    return db ('schemes')
+    .where({ id })
+    .first()
+};
+
+const findSteps= id => {
+    return db('steps')
+    .select('schemes.scheme_name',
+     'steps.step_number', 
+     'steps.instructions')
+    .join('schemes', 'schemes.id', '=', 'steps.scheme_id')
+        .where({ 'scheme_id': id })
+};
+
+const add = schemes => {
+    return db('schemes')
+        .insert(schemes)
+        .where();
+};
+
+const update = (changes, id) => {
+    return db('schemes')
+        .where({ id })
+        .update(changes)
+        .then(()=> {
+            return findById(id);
+        })
+};
+
+const remove = id => {
+    return db('schemes')
+        .where({ id })
+        .del();
+};
+
+const addStep = (step, scheme_id) => {
+    return db('steps')
+        .insert(step)
+        .where(scheme_id)
+        
+};
+
 module.exports = {
     find,
     findById,
@@ -8,46 +57,4 @@ module.exports = {
     update,
     remove,
     addStep
-};
-
-function find() {
-    return db('schemes');
-};
-
-function findById(id) {
-    return db ('schemes')
-    .where({ id })
-    .first()
-    // .then(scheme => { scheme ? scheme : null })
-};
-
-function findSteps(id) {
-    return db('schemes as s')
-        .join('steps as st', 's.id', 'st.scheme_id')
-        .select('s.scheme_name as Scheme', 'st.step_number as Step', 'st.instructions as Instructions')
-        .where('s.id', id)
-        .orderBy('st.step_number');
-};
-
-function add(scheme) {
-    return db('schemes')
-        .insert(scheme, 'id');
-};
-
-function update(changes, id) {
-    return db('schemes')
-        .where({ id })
-        .update(changes);
-};
-
-function remove(id) {
-    return db('schemes')
-        .where({ id })
-        .del();
-};
-
-function addStep(step, scheme_id) {
-    return db('steps')
-        .where('scheme_id', scheme_id)
-        .insert(step, 'id');
 };
